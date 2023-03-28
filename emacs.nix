763a36adde8d3d6f17232fc6aa683a65cde1f503
DPDmancul <davide.peressoni@tuta.io>
2023-03-28 21:06:45 +0200

    better emacs config

--- emacs.nix
+++ emacs.nix
@@ -3,7 +3,7 @@
   nativeBuildInputs = with pkgs; oldAttrs.nativeBuildInputs ++ [
     (symlinkJoin {
       name = "emacs";
-      paths = [ (emacs-nox.pkgs.withPackages (epkgs: [ epkgs.evil ])) ];
+      paths = [ (emacs-nox.pkgs.withPackages (epkgs: [ epkgs.evil epkgs.evil-terminal-cursor-changer ])) ];
       buildInputs = [ makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/emacs \
@@ -15,10 +15,10 @@
 
           (menu-bar-mode -1)
 
-          (if (not (display-graphic-p))
-              (progn (set-terminal-parameter nil 'background-mode 'light)
-                     ;;(load-theme 'tsdh-light)
-                     ))
+          (unless (display-graphic-p))
+            (progn (set-terminal-parameter nil 'background-mode 'light)
+                   ;;(load-theme 'tsdh-light)
+                   )
 
           (custom-set-variables
             '(xterm-mouse-mode t)
@@ -30,6 +30,10 @@
                                  ;; support for terminal
                                  (define-key (current-local-map) (kbd "C-c ,") (lookup-key (current-local-map) (kbd "C-c C-,")))
                                  (define-key (current-local-map) (kbd "C-c .") (lookup-key (current-local-map) (kbd "C-c C-.")))
+                                 (display-line-numbers-mode)
+                                 (setq display-line-numbers 'relative)
+                                 (evil-insert-state)
+                                 (activate-input-method "Agda")
                     ))
 
           (run-with-idle-timer 60 t #'(lambda () (save-some-buffers t nil)))
@@ -37,6 +41,10 @@
           (require 'evil)
           (setq evil-want-fine-undo 't)
           (evil-mode 1)
+          (unless (display-graphic-p)
+            (require 'evil-terminal-cursor-changer)
+            (evil-terminal-cursor-changer-activate))
+          (setq evil-insert-state-cursor 'bar)
         ''}'"
       '';
     })
